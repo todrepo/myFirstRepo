b38054c382982d8ba731e0624df5ca5602bb5bfe
Thierry Debionne <thierry.debionne@ge.com>
2023-12-04 10:23:58 +0100

    Project creation

---
+++ tests/example.spec.ts
@@ -0,0 +1,27 @@
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+});
+
+test('get started link', async ({ page }) => {
+  await test.step('open main page step title', async () => {
+    await page.goto('https://playwright.dev/')});
+  
+  // Click the get started link.
+  await test.step('Click link step title', async () => {
+    await page.getByRole('link', { name: 'Get started' }).click()});
+
+  // Expects page to have a heading with the name of Installation.
+  await test.step('Check content of open page step title', async () => {
+    await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible()});
+});
+
+test('get doc link',async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+  await page.getByRole('link', { name: 'Docs'}).click();
+  await expect(page.getByRole('heading', {name: 'Installation' })).toBeVisible();
+})
